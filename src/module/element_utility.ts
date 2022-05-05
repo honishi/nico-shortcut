5703b0f57c3de8ca89631af6d73844f170f9a4ce
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-05 20:44:02 +0900

    add comment

--- src/module/element_utility.ts
+++ src/module/element_utility.ts
@@ -14,20 +14,26 @@ export const clickElement = (element: Element | null | undefined) => {
 
 export const clickMenuButton = (divClass: string, sectionClass: string, buttonIndex: number) => {
     const timeout = 300
+
+    // 1. Click setting button.
     const button = document.querySelector("button[class*='___setting-button___']")
     clickElement(button)
+
     setTimeout(() => {
+        // 2. Click menu button.
         const div = document.querySelector(`div[class^=${divClass}]`)
         const button = div?.querySelector('button')
         clickElement(button)
 
         setTimeout(() => {
+            // 3. Click item button.
             const section = document.querySelector(`section[class^=${sectionClass}]`)
             const buttons = section?.querySelectorAll('button')
             if (buttons == null) return
             clickElement(buttons[buttonIndex])
 
             setTimeout(() => {
+                // 4. Click setting button to close.
                 const button = document.querySelector("button[class*='___setting-button___']")
                 clickElement(button)
             }, timeout)
