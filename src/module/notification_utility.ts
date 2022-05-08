b972e65a8e58db71d5e63869a3fadf22601770ab
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-09 03:18:50 +0900

    adjust notification duration

--- src/module/notification_utility.ts
+++ src/module/notification_utility.ts
@@ -1,6 +1,6 @@
 import Notiflix from "notiflix";
 
-export const showNotification = (text: string, width: number = 380, duration: number = 500) => {
+export const showNotification = (text: string, width: number = 380, duration: number = 1000) => {
     // https://github.com/notiflix/Notiflix
     Notiflix.Notify.info(
         text,
