3955fad335354b7e33b70cc8bc38f1560cf202e0
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-10 19:05:12 +0900

    remove unused method

--- src/module/handle_message.ts
+++ src/module/handle_message.ts
@@ -8,9 +8,6 @@ export function handleMessage(message: any) {
         case 'open_url':
             openUrl(parameter)
             break
-        case 'xxx':
-            // moveToTab()
-            break
         default:
             break
     }
@@ -20,30 +17,4 @@ function openUrl(url: string) {
     chrome.tabs.create(
         {url: url},
         () => null)
-}
-
-function moveToTab() {
-    chrome.system.display.getInfo((results) => {
-        console.log(results)
-        const info = results[0]
-        const height = info.bounds.height
-        const width = info.bounds.width
-        // https://github.com/hukurouo/popupWindow/blob/master/background.js
-        chrome.tabs.query({active: true}, tab => { // getCurrent(tab => {
-            // 現タブを閉じて、
-            console.log(tab)
-            console.log(tab[0].id)
-            const tabId = tab[0].id
-            if (tabId == null) return
-            chrome.tabs.remove(tabId).then(r => null)
-            // 新たにポップアップウィンドウでURLを開く
-            chrome.windows.create({
-                url: tab[0].url,
-                type: 'normal',
-                width: width / 3, height: height / 2,
-                focused: true
-            }, () => {
-            })
-        })
-    })
 }
