afcb34fb52a17fd9bc214553247895e4abc31edc
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-14 02:14:02 +0900

    format .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,15 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    webextensions: true,
   },
-  extends: [
-    'standard',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["standard", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {}
-}
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
