a56da57cef7148566cb6816eadf58fc89672df9c
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2022-05-17 00:11:34 +0900

    refactor comment handling

--- src/module/page-controller.ts
+++ src/module/page-controller.ts
@@ -12,7 +12,96 @@ export function clickElement(element: Element | null | undefined) {
   element.click();
 }
 
-export function clickMenuButton(divClass: string, sectionClass: string, buttonIndex: number) {
+export function buttonToggleState(divClass: string, buttonClass: string): boolean {
+  const div = document.querySelector(`div[class^='${divClass}']`);
+  const button = div?.querySelector(`button[class^='${buttonClass}']`);
+  return button?.getAttribute("data-toggle-state") === "true";
+}
+
+//
+// Playback Control
+//
+export function getPlayButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___play-button___']");
+}
+
+export function getBackButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___back-button___']");
+}
+
+export function getForwardButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___forward-button___']");
+}
+
+export function getHeadButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___head-button___']");
+}
+
+export function getLiveButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___live-button___']");
+}
+
+export function isPlaying(): boolean {
+  return getPlayButton()?.isDataToggleStateTrue() ?? false;
+}
+
+//
+// Comment Control
+//
+export function getCommentButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___comment-button___']");
+}
+
+export function isCommentEnabled(): boolean {
+  return getCommentButton()?.isDataToggleStateTrue() ?? false;
+}
+
+//
+// Misc Control
+//
+export function getFullscreenButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___fullscreen-button___']");
+}
+
+export function getReloadButton(): HTMLButtonElement | null {
+  return queryButton("button[class^='___reload-button___']");
+}
+
+export function isFullscreenEnabled(): boolean {
+  return getFullscreenButton()?.isDataToggleStateTrue() ?? false;
+}
+
+//
+// Menu Control
+//
+export function getSettingButton(): HTMLButtonElement | null {
+  return queryButton("button[class*='___setting-button___']");
+}
+
+export function changePlaybackRate(buttonIndex: number) {
+  clickMenuButton(
+    "___video-playback-rate-menu-button-field___",
+    "___video-playback-rate-select-menu___",
+    buttonIndex
+  );
+}
+
+export function changeCommentTransparency(buttonIndex: number) {
+  clickMenuButton(
+    "___comment-transparency-menu-button-field___",
+    "___comment-transparency-select-menu___",
+    buttonIndex
+  );
+}
+
+//
+// Internal Methods
+//
+function queryButton(selector: string): HTMLButtonElement | null {
+  return document.querySelector(selector) as HTMLButtonElement;
+}
+
+function clickMenuButton(divClass: string, sectionClass: string, buttonIndex: number) {
   const timeout = 300;
 
   // 1. Click setting button.
@@ -45,60 +134,17 @@ export function clickMenuButton(divClass: string, sectionClass: string, buttonIn
   }, timeout);
 }
 
-export function buttonToggleState(divClass: string, buttonClass: string): boolean {
-  const div = document.querySelector(`div[class^='${divClass}']`);
-  const button = div?.querySelector(`button[class^='${buttonClass}']`);
-  return button?.getAttribute("data-toggle-state") === "true";
-}
-
 //
-// Playback Control
+// Utility Extension
 //
-export function getPlayButton(): HTMLButtonElement | null {
-  const button = document.querySelector("button[class^='___play-button___']");
-  return button as HTMLButtonElement;
+/* eslint-disable no-unused-vars */
+declare global {
+  interface HTMLButtonElement {
+    isDataToggleStateTrue(): boolean;
+  }
 }
+/* eslint-enable no-unused-vars */
 
-export function getBackButton(): HTMLButtonElement | null {
-  const button = document.querySelector("button[class^='___back-button___']");
-  return button as HTMLButtonElement;
-}
-
-export function getForwardButton(): HTMLButtonElement | null {
-  const button = document.querySelector("button[class^='___forward-button___']");
-  return button as HTMLButtonElement;
-}
-
-export function getHeadButton(): HTMLButtonElement | null {
-  const button = document.querySelector("button[class^='___head-button___']");
-  return button as HTMLButtonElement;
-}
-
-export function getLiveButton(): HTMLButtonElement | null {
-  const button = document.querySelector("button[class^='___live-button___']");
-  return button as HTMLButtonElement;
-}
-
-export function isPlaying(): boolean {
-  const playButton = getPlayButton();
-  return playButton?.getAttribute("data-toggle-state") === "true";
-}
-
-//
-// Menu Control
-//
-export function changePlaybackRate(buttonIndex: number) {
-  clickMenuButton(
-    "___video-playback-rate-menu-button-field___",
-    "___video-playback-rate-select-menu___",
-    buttonIndex
-  );
-}
-
-//
-// Internal Methods
-//
-function getSettingButton(): HTMLButtonElement | null {
-  const button = document.querySelector("button[class*='___setting-button___']");
-  return button as HTMLButtonElement;
-}
+HTMLButtonElement.prototype.isDataToggleStateTrue = function () {
+  return this.getAttribute("data-toggle-state") === "true";
+};
